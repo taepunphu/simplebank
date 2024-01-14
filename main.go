aad365c35ca5a1cacfa9e84d17df655d78a0c0f4
Kiattisak Phanphu <43429886+taepunphu@users.noreply.github.com>
2024-01-14 10:59:26 +0700

    feat(config): add load config env

--- main.go
+++ main.go
@@ -6,18 +6,19 @@ import (
 
 	"github.com/taepunphu/simplebank/api"
 	db "github.com/taepunphu/simplebank/db/sqlc"
+	"github.com/taepunphu/simplebank/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
 
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config env: ", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db: ", err)
 	}
@@ -25,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
